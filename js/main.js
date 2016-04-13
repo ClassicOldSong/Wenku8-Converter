67114819a6bd46fcd41239264cf6623d201e0841
Yukino Song <syqlds@126.com>
2016-04-14 02:34:36 +0800

    Fixed FireFox support, removed unnecessary image loading, added logs

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@
 	let txtBtn = document.querySelector('#txtBtn');
 	let mergeFile = document.querySelector('#mergeFile');
 	let zipFile = document.querySelector('#zipFile');
-
-	let sortNumber = function(a, b) {
-		return a - b;
-	};
+	let outPut = document.querySelector('#outPut');
 
 	mergeFile.onchange = () => {
 		if (!mergeFile.checked) {
@@ -21,73 +18,107 @@
 		}
 	};
 
+	let sortNumber = function(a, b) {
+		return a - b;
+	};
+
+	let pageCreation = function(content) {
+		let tempPage = document.createElement('div');
+		tempPage.innerHTML = new TextDecoder('gb18030').decode(content).replace(/<img(.*)??>/ig, '');
+		return tempPage;
+	};
+
+	let logger = {
+		log(content) {
+			outPut.textContent = `[Log] ${content}\n` + outPut.textContent;
+		},
+		err(content) {
+			outPut.textContent = `[Error] ${content}\n` + outPut.textContent;
+		},
+		clear() {
+			outPut.textContent = '';
+		}
+	};
+
 	zipBtn.onclick = () => {
 		let zipLoader = document.createElement('input');
 		zipLoader.type = 'file';
 		zipLoader.accept = '.zip';
 		zipLoader.addEventListener('change', function(e) {
+			logger.clear();
+			logger.log('Starting conversion...');
 			let fileReader = new FileReader();
 			fileReader.onload = event => {
 				let title = '';
 				let chapters = [];
-				let contentZip = new JSZip(event.target.result);
-				let indexPage = document.createElement('div');
-				indexPage.innerHTML = new TextDecoder('gb18030').decode(contentZip.file('index.htm').asUint8Array());
-				title = indexPage.querySelector('#title').innerText.replace(/[ ~!@#$%\^\+\*&\\\/\?\|:\.<>{}()';="\u200f\u200e]/g, ' ');
-				for (let i in contentZip.files) {
-					if (i !== 'index.htm' && i !== 'page.css') {
-						chapters.push(i.split('.')[0]);
+				try {
+					let contentZip = new JSZip(event.target.result);
+					let indexPage = pageCreation(contentZip.file('index.htm').asUint8Array());
+					title = indexPage.querySelector('#title').textContent.replace(/[ ~!@#$%\^\+\*&\\\/\?\|:\.<>{}()';="\u200f\u200e]/g, ' ');
+					indexPage = null;
+					logger.log(`Book Name: ${title}`);
+					for (let i in contentZip.files) {
+						if (i !== 'index.htm' && i !== 'page.css') {
+							chapters.push(i.split('.')[0]);
+						}
 					}
-				}
-				chapters.sort(sortNumber);
-				if (mergeFile.checked) {
-					let content = '';
-					for (let i in chapters) {
-						let page = document.createElement('div');
-						page.innerHTML = new TextDecoder('gb18030').decode(contentZip.file(chapters[i]+'.htm').asUint8Array());
-						content += page.querySelector('#contentmain').innerText.replace(/\n\n\n/g, '');
-					}
-					if (zipFile.checked) {
+					logger.log(`Chapters read: ${chapters.length}`);
+					chapters.sort(sortNumber);
+					if (mergeFile.checked) {
+						let content = '';
+						for (let i in chapters) {
+							let page = pageCreation(contentZip.file(chapters[i]+'.htm').asUint8Array());
+							content += page.querySelector('#contentmain').textContent.replace(/\n\n\n/g, '');
+							page = null;
+						}
+						if (zipFile.checked) {
+							logger.log('Packaging...');
+							let outputZip = new JSZip();
+							outputZip.file(title + '.txt', content);
+							saveAs(outputZip.generate({
+								type: 'blob',
+								compression: 'DEFLATE',
+								compressionOptions: {
+									level: 9
+								}
+							}), title + '_converted.zip');
+						} else {
+							saveAs(new Blob([content], {type: 'plain/text'}), title + '_converted.txt');
+						}
+					} else {
 						let outputZip = new JSZip();
-						outputZip.file(title + '.txt', content);
+						let chapterCount = (function() {
+							let string = chapters.length.toString();
+							return string.length - 1;
+						})();
+						for (let i in chapters) {
+							let chapterNumber = parseInt(i) + 1;
+							let page = pageCreation(contentZip.file(chapters[i]+'.htm').asUint8Array());
+							let filename = '';
+							filename = chapterNumber + ' ' + page.querySelector('#title').textContent.replace(/[ ~!@#$%\^\+\*&\\\/\?\|:\.<>{}()';="\u200f\u200e]/g, ' ') + '.txt';
+							if (chapterNumber <= Math.pow(10, chapterCount)) {
+								let numString = chapterNumber.toString();
+								let diff = chapterCount - numString.length + 1;
+								for (let j = 0; j < diff; j++) {
+									filename = '0' + filename;
+								}
+							}
+							outputZip.file(filename, page.querySelector('#contentmain').textContent.replace(/\n\n\n/g, ''));
+							page = null;
+						}
+						logger.log('Packaging...');
 						saveAs(outputZip.generate({
 							type: 'blob',
 							compression: 'DEFLATE',
 							compressionOptions: {
 								level: 9
 							}
-						}), title + '_converted');
-					} else {
-						saveAs(new Blob([content], {type: '.txt'}), title + '_converted.txt');
+						}), title + '_converted.zip');
 					}
-				} else {
-					let outputZip = new JSZip();
-					let chapterCount = (function() {
-						let string = chapters.length.toString();
-						return string.length - 1;
-					})();
-					for (let i in chapters) {
-						let page = document.createElement('div');
-						page.innerHTML = new TextDecoder('gb18030').decode(contentZip.file(chapters[i]+'.htm').asUint8Array());
-						let filename = '';
-						let chapterNumber = parseInt(i) + 1;
-						filename = chapterNumber + ' ' + page.querySelector('#title').innerText.replace(/[ ~!@#$%\^\+\*&\\\/\?\|:\.<>{}()';="\u200f\u200e]/g, ' ') + '.txt';
-						if (chapterNumber <= Math.pow(10, chapterCount)) {
-							let numString = chapterNumber.toString();
-							let diff = chapterCount - numString.length + 1;
-							for (let j = 0; j < diff; j++) {
-								filename = '0' + filename;
-							}
-						}
-						outputZip.file(filename, page.querySelector('#contentmain').innerText.replace(/\n\n\n/g, ''));
-					}
-					saveAs(outputZip.generate({
-						type: 'blob',
-						compression: 'DEFLATE',
-						compressionOptions: {
-							level: 9
-						}
-					}), title + '_converted');
+					logger.log('Conversion complete!');
+				} catch(err) {
+					logger.err('An error occurred.');
+					logger.log('Conversion aborted.');
 				}
 			};
 			fileReader.readAsArrayBuffer(e.target.files[0]);
@@ -100,22 +131,30 @@
 		txtLoader.type = 'file';
 		txtLoader.accept = '.txt';
 		txtLoader.addEventListener('change', function(e) {
+			logger.clear();
+			logger.log('Starting conversion...');
 			let fileReader = new FileReader();
 			let fileName = e.target.files[0].name.split('.')[0];
 			fileReader.onload = event => {
-				let u8arr = new Uint8Array(event.target.result);
-				if (zipFile.checked) {
-					let outputZip = new JSZip();
-					outputZip.file(fileName + '.txt', new TextDecoder('gb18030').decode(u8arr));
-					saveAs(outputZip.generate({
-						type: 'blob',
-						compression: 'DEFLATE',
-						compressionOptions: {
-							level: 9
-						}
-					}), fileName + '_converted');
-				} else {
-					saveAs(new Blob([new TextDecoder('gb18030').decode(u8arr)], {type: '.txt'}), fileName + '_converted.txt');
+				try {
+					let u8arr = new Uint8Array(event.target.result);
+					if (zipFile.checked) {
+						let outputZip = new JSZip();
+						outputZip.file(fileName + '.txt', new TextDecoder('gb18030').decode(u8arr));
+						saveAs(outputZip.generate({
+							type: 'blob',
+							compression: 'DEFLATE',
+							compressionOptions: {
+								level: 9
+							}
+						}), fileName + '_converted.zip');
+					} else {
+						saveAs(new Blob([new TextDecoder('gb18030').decode(u8arr)], {type: 'plain/text'}), fileName + '_converted.txt');
+					}
+					logger.log('Conversion complete!');
+				} catch(err) {
+					logger.err('An error occurred.');
+					logger.log('Conversion aborted.');
 				}
 			};
 			fileReader.readAsArrayBuffer(e.target.files[0]);
